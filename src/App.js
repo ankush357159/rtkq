767efb06af9f8638570dd48a74a67dc8f66e2643
Ankush <ankush357159@gmail.com>
2022-06-15 21:20:42 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useSelector} from "react-redux"
 
 function App() {
+  const userList = useSelector((state)=> state.users.value)
+  console.log(userList)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="addUser">
+        <input type="text" placeholder='Name...' />
+        <input type="text" placeholder='Username...' />
+        <button>Add User</button>
+      </div>
+
+      <div className="displayUsers">
+        {userList.map((user)=> (
+          <div key={user.id}>
+            <h1>{user.name}</h1>
+          </div>
+        ))}
+      </div>
+     
     </div>
   );
 }
